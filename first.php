c1161aea4f9d4dc17c5cb7a5b9af356a24699cf5
railtoom <railtoom@greeny.cs.tlu.ee>
2016-09-08 15:16:08 +0300

    Nimi

--- first.php
+++ first.php
@@ -1,1 +1,15 @@
-tere
+<?php
+
+	$firstName="Raily";
+	$lastName="Toomingas";
+
+	//käsk echo trükib välja muutuja väärtus
+	//echo $firstName;
+	//echo $lastName;
+
+	
+	echo $firstName." ".$lastName;
+	
+	
+	
+?>
